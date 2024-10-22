d686ce30b34858e1a0ac6ed513bce5e42c4e4c95
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-22 13:24:56 +0300

    test

--- crave.sh
+++ crave.sh
@@ -1,11 +1,25 @@
-crave run  --no-patch -- "rm -rf .repo/local_manifests; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs; \
-repo sync; \
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs; \
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --fifteen; \
-git clone https://github.com/keosh1/local_manifests.git --depth 1 -b a15 .repo/local_manifests; \
-/opt/crave/resync.sh; \
-source build/envsetup.sh; \
-curl https://raw.githubusercontent.com/keosh1/local_manifests/refs/heads/a15/repopicks.sh | bash; \
-lunch yaap_mondrian-userdebug; \ 
-m yaap;
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone https://github.com/keosh1/local_manifests.git --depth 1 -b a15 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+source build/envsetup.sh
+curl https://raw.githubusercontent.com/keosh1/local_manifests/refs/heads/a15/repopicks.sh | bash
+
+#build the rom
+lunch yaap_mondrian-userdebug
+m yaap
