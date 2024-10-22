17c0458da051e73eb86adc40e15a88d3d521d16b
Keosh <106485828+keosh1@users.noreply.github.com>
2024-10-22 12:31:01 +0300

    a15

--- repopicks.sh
+++ repopicks.sh
@@ -4,29 +4,31 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
-# hardware/xiaomi
-repopick -P hardware/xiaomi 352657 # Add dummy sensors sub HAL
-repopick -P hardware/xiaomi 352658 # sensors: Make sensor batch function virtual
-repopick -P hardware/xiaomi 352659 # sensors: Make sensor run function virtual
-repopick -P hardware/xiaomi 352660 # sensors: Make sensor flush function virtual
-repopick -P hardware/xiaomi 352661 # sensors: Make sensor set mode operation function virtual
-repopick -P hardware/xiaomi 352662 # sensors: Move one shot sensor out of main class
-repopick -P hardware/xiaomi 352663 # sensors: Fix locking around setOperationMode and activate
-repopick -P hardware/xiaomi 352664 # sensors: Create sysfs polling one shot sensor
-repopick -P hardware/xiaomi 392967 # sensors: Let the reading of poll fd be configurable
-repopick -P hardware/xiaomi 392968 # sensors: Add SysfsPollingOneShotSensor constructor without enable path
-repopick -P hardware/xiaomi 392969 # sensors: Add udfps long press sensor using SysfsPollingOneShotSensor
-repopick -P hardware/xiaomi 352665 # sensors: Handle fod_pressed_state without coordinates
-repopick -P hardware/xiaomi 363160 # hidl: biometrics: fingerprint: Add enroll methods to udfps handler
-repopick -P hardware/xiaomi 392965 # vibrator: effect: Create double click effect from click if necessary
-repopick -P hardware/xiaomi 392966 # vibrator: effect: Fallback to click if an effect is missing
+# hardware/qcom-caf/sm8450/audio/primary-hal
+changes=(
+405881 # hal: Guard primary audio HAL in case target provides prebuilt
+)
+repopick -f -P hardware/qcom-caf/sm8450/audio/primary-hal ${changes[@]}&
 
-# frameworks/base
-#repopick -P frameworks/base 386158 # Add 5G Ultra Wideband icon carrier config keys
-#repopick -P frameworks/base 386159 # Fix default values for 5G Ultra Wideband icon carrier config keys
+# hardware/xiaomi
+changes=(
+405259 # fingerprint: Move UDFPS handler and extension out of HIDL
+404720 # aidl: fingerprint: Initial stub service
+404721 # aidl: fingerprint: Initial Xiaomi implementation
+)
+repopick -f -P hardware/xiaomi ${changes[@]}&
 
 # vendor/lineage
-repopick -P vendor/lineage 367044 # android: merge_dtbs: Respect miboard-id while merging
+changes=(
+367044 # android: merge_dtbs: Respect miboard-id while merging
+)
+repopick -f -P vendor/lineage ${changes[@]}&
 
-# vendor/qcom/opensource/vibrator
-repopick -P vendor/qcom/opensource/vibrator 392952 # vibrator: Use a better check for primitive effects
+wait
+
+wget https://raw.githubusercontent.com/luk1337/lineageos-rootdir/refs/heads/lineage-22.0/picks
+wget https://raw.githubusercontent.com/luk1337/lineageos-rootdir/refs/heads/lineage-22.0/gen_chain
+chmod +x picks
+chmod +x gen_chain
+bash ./picks
+rm picks gen_chain
